c90722196c48b79ce328144e5cdea5ae0cea2a25
Hiro11190422 <hiroshi01.toyoshima@edu.pref.shizuoka.jp>
2019-08-07 22:24:09 +0900

    update files 201908072224

--- js/gps.js
+++ js/gps.js
@@ -1,59 +1,69 @@
 if (navigator.geolocation) {
-    //Geolocation APIが利用できる場合
-    //現在位置を取得する
-    navigator.geolocation.watchPosition(
+  //Geolocation APIが利用できる場合
+  //現在位置を取得する
+  navigator.geolocation.watchPosition(
 
-        //getCurrentPositionの第１引数
-        function successFunc(position) {
-            // mobile backendアプリとの連携
-            var ncmb = new NCMB("3bfa8718ce052e1ca1f992e74e255e22e27e176e9b709358b0bb46500ec2d90a", "81d6c842a24bbe4af6a517da7d4c9864e52ef50c3fb2722d1b51b332f32f67e2");
-            // クラスのTestClassを作成
-            var gpsLogClass = ncmb.DataStore("gpsLog");
+    //getCurrentPositionの第１引数
+    function successFunc(position) {
+      // mobile backendアプリとの連携
+      var ncmb = new NCMB("3bfa8718ce052e1ca1f992e74e255e22e27e176e9b709358b0bb46500ec2d90a", "81d6c842a24bbe4af6a517da7d4c9864e52ef50c3fb2722d1b51b332f32f67e2");
+      // クラスのTestClassを作成
+      var gpsLogClass = ncmb.DataStore("gpsLog");
 
-            // データストアへの登録
-            var gpsLogClass = new gpsLogClass();
-            gpsLogClass.set("Lat", position.coords.latitude);
-            gpsLogClass.set("Lng", position.coords.longitude);
-            gpsLogClass.save()
-                .then(function() {
-                    // 保存に成功した場合の処理
-                    // alert("sending")
-                    var sending=document.getElementsByIdName('sending');
-                    sending.innerHTML+="sending";
-                })
-                .catch(function(err) {
-                    // 保存に失敗した場合の処理
-                    // alert("error")
-                });
-        },
+      // データストアへの登録
+      var gpsLogClass = new gpsLogClass();
+      gpsLogClass.set("Lat", position.coords.latitude);
+      gpsLogClass.set("Lng", position.coords.longitude);
+      gpsLogClass.save()
+        .then(function() {
+          // 保存に成功した場合の処理
+          // alert("sending");
+          var geo_text = "緯度:" + position.coords.latitude + "<br/>";
+          geo_text += "経度:" + position.coords.longitude + "<br/>";
+          geo_text += "高度:" + position.coords.altitude + "<br/>";
+          geo_text += "位置精度:" + position.coords.accuracy + "<br/>";
+          geo_text += "高度精度:" + position.coords.altitudeAccuracy + "<br/>";
+          geo_text += "移動方向:" + position.coords.heading + "<br/>";
+          geo_text += "速度:" + position.coords.speed + "<br/>";
 
-        //getCurrentPositionの第２引数
-        function errorFunc(error) {
-            var errorInfo = [
-                "0: UNKNOWN_ERROR 原因不明のエラーが発生しました",
-                "1: PERMISSION_DENIED 利用者が位置情報の取得を許可されませんでした",
-                "2: POSITION_UNAVAILABLE 電波状況などで位置情報が取得できませんでした",
-                "3: TIMEOUT 位置情報の取得に時間がかかり取得できませんでした"
-            ];
+          document.getElementById('sending').innerHTML = geo_text;
 
-            var errorMessage = error.code;
+        })
+        .catch(function(err) {
+          // 保存に失敗した場合の処理
+          // alert("error")
+        });
 
-            alert(errorMessage);
+        document.getElementById('sending').innerHTML = "xxx";
+    },
 
-        },
+    //getCurrentPositionの第２引数
+    function errorFunc(error) {
+      var errorInfo = [
+        "0: UNKNOWN_ERROR 原因不明のエラーが発生しました",
+        "1: PERMISSION_DENIED 利用者が位置情報の取得を許可されませんでした",
+        "2: POSITION_UNAVAILABLE 電波状況などで位置情報が取得できませんでした",
+        "3: TIMEOUT 位置情報の取得に時間がかかり取得できませんでした"
+      ];
 
-        {
-            //getCurrentPositionの第３引数
-            "enableHighAccuracy": true,
-            "timeout": 8000,
-            "maximumAge": 2000,
-        }
-    );
+      var errorMessage = error.code;
+
+      alert(errorMessage);
+
+    },
+
+    {
+      //getCurrentPositionの第３引数
+      "enableHighAccuracy": true,
+      "timeout": 8000,
+      "maximumAge": 2000,
+    }
+  );
 
 
 } else {
-    //Geolocation APIが利用できない場合
-    alert("現在利用している端末では、位置情報を取得できません。")
+  //Geolocation APIが利用できない場合
+  alert("現在利用している端末では、位置情報を取得できません。")
 }
 
 //setTimeout("location.reload()",5000);
