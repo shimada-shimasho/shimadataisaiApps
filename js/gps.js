0c2a17bfaa69d0e4283b6014f116850ad30743e1
Hiro11190422 <hiroshi01.toyoshima@edu.pref.shizuoka.jp>
2019-08-24 15:40:32 +0900

    Update gps.js

--- js/gps.js
+++ js/gps.js
@@ -14,65 +14,67 @@ if (navigator.geolocation) {
   //現在位置を取得する
   navigator.geolocation.watchPosition(
     //getCurrentPositionの第１引数
-    function successFunc(position) {
+    successFunc(position) ,
 
-      var geo_text = "緯度:" + position.coords.latitude + "<br/>";
-      geo_text += "経度:" + position.coords.longitude + "<br/>";
-      geo_text += "高度:" + position.coords.altitude + "<br/>";
-      geo_text += "位置精度:" + position.coords.accuracy + "<br/>";
-      geo_text += "高度精度:" + position.coords.altitudeAccuracy + "<br/>";
-      geo_text += "移動方向:" + position.coords.heading + "<br/>";
-      geo_text += "速度:" + position.coords.speed + "<br/>";
+    //getCurrentPositionの第２引数
+    function errorFunc(error) {
+      var errorInfo = [
+        "0: UNKNOWN_ERROR 原因不明のエラーが発生しました",
+        "1: PERMISSION_DENIED 利用者が位置情報の取得を許可されませんでした",
+        "2: POSITION_UNAVAILABLE 電波状況などで位置情報が取得できませんでした",
+        "3: TIMEOUT 位置情報の取得に時間がかかり取得できませんでした"
+      ];
 
-      document.getElementById('geolocation_data').innerHTML = geo_text;
+      var errorMessage = error.code;
 
-      var checkTime = new date();
-      //checkTime=checkTime-300000;
+      document.getElementById('geolocation_data').innerHTML = "geolocation_error[ " + errorInfo[error.code] + " ]";
 
-      if (checkTime >= updateTime) {
-        gpsLogClass.set("Lat", position.coords.latitude);
-        gpsLogClass.set("Lng", position.coords.longitude);
-        gpsLogClass.save()
-          .then(function() {
-            // 保存に成功した場合の処理
-            var nowTime = new date();
-            document.getElementById('sending').innerHTML = "data_uplode_success" + nowTime;
-            updateTime = nowTime;
-          })
-          .catch(function(err) {
-              var nowTime = new date();
-              document.getElementById('sending').innerHTML = "data_uplode_error" + nowTime;
-            }
-          }
-      },
+    },
 
-      //getCurrentPositionの第２引数
-      function errorFunc(error) {
-        var errorInfo = [
-          "0: UNKNOWN_ERROR 原因不明のエラーが発生しました",
-          "1: PERMISSION_DENIED 利用者が位置情報の取得を許可されませんでした",
-          "2: POSITION_UNAVAILABLE 電波状況などで位置情報が取得できませんでした",
-          "3: TIMEOUT 位置情報の取得に時間がかかり取得できませんでした"
-        ];
-
-        var errorMessage = error.code;
-
-        document.getElementById('geolocation_data').innerHTML = "geolocation_error[ " + errorInfo[error.code] + " ]";
-
-      },
-
-      {
-        //getCurrentPositionの第３引数
-        "enableHighAccuracy": true,
-        "timeout": 3000,
-        "maximumAge": 2000,
-      });
+    //getCurrentPositionの第３引数
+    {
+      "enableHighAccuracy": true,
+      "timeout": 3000,
+      "maximumAge": 2000,
+    });
 
 
-  }
-  else {
-    //Geolocation APIが利用できない場合
-    document.getElementById('geolocation_data').innerHTML = "現在利用している端末では、位置情報を取得できません。";
-  }
+} else {
+  //Geolocation APIが利用できない場合
+  document.getElementById('geolocation_data').innerHTML = "現在利用している端末では、位置情報を取得できません。";
+}
 
-  //setTimeout("location.reload()",5000);
+//setTimeout("location.reload()",5000);
+
+//getCurrentPositionの第１引数
+function successFunc(position) {
+  var geo_text = "緯度:" + position.coords.latitude + "<br/>";
+  geo_text += "経度:" + position.coords.longitude + "<br/>";
+  geo_text += "高度:" + position.coords.altitude + "<br/>";
+  geo_text += "位置精度:" + position.coords.accuracy + "<br/>";
+  geo_text += "高度精度:" + position.coords.altitudeAccuracy + "<br/>";
+  geo_text += "移動方向:" + position.coords.heading + "<br/>";
+  geo_text += "速度:" + position.coords.speed + "<br/>";
+
+  document.getElementById('geolocation_data').innerHTML = geo_text;
+
+  var checkTime = new date();
+  //checkTime=checkTime-300000;
+
+  if (checkTime >= updateTime) {
+    gpsLogClass.set("Lat", position.coords.latitude);
+    gpsLogClass.set("Lng", position.coords.longitude);
+    gpsLogClass.save()
+      .then(function() {
+        // 保存に成功した場合の処理
+        var nowTime = new date();
+        document.getElementById('sending').innerHTML = "data_uplode_success" + nowTime;
+        updateTime = nowTime;
+      })
+      .catch(function(err) {
+          var nowTime = new date();
+          document.getElementById('sending').innerHTML = "data_uplode_error" + nowTime;
+        }
+      }
+
+}
