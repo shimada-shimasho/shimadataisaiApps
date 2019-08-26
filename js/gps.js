90dce9953be9e32af802812b368a32498992bed1
Hiro11190422 <hiroshi01.toyoshima@edu.pref.shizuoka.jp>
2019-08-26 10:34:56 +0900

    Update gps.js

--- js/gps.js
+++ js/gps.js
@@ -30,7 +30,8 @@ if (navigator.geolocation) {
 
       //時間をチェックし５分以上時間が経っていればデータアップロード
       var checkTime = new Date();
-      checkTime=checkTime-300000;
+      // checkTime=checkTime-300000;
+      
 
       if (checkTime >= updateTime) {
         // if(tryFlg==0){
